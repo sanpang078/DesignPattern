58eb0552112c9ce10ae9974f293ea7624c564ba8
sanpang <sanpang0309@163.com>
2024-06-19 15:52:43 +0800

    代理模式-第三版 动态代理

---
+++ src/test/java/org/example/structure/proxy/MetricsCollectorProxyTest.java
@@ -0,0 +1,10 @@
+package org.example.structure.proxy;
+
+public class MetricsCollectorProxyTest {
+    public static void main(String[] args) {
+        MetricsCollectorProxy proxy = new MetricsCollectorProxy();
+        IUserController userController = (IUserController) proxy.createProxy(new UserController());
+        userController.register("111122223333","sanpang");
+
+    }
+}
